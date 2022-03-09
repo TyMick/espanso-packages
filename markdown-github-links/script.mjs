57d57d5f26d8cd5cb16f6be08d11ca3e96766e35
Ty Mick <ty.mick@faithlife.com>
2022-03-09 09:24:00 -0800

    Add PR commits

--- markdown-github-links/script.mjs
+++ markdown-github-links/script.mjs
@@ -3,8 +3,13 @@ const path = url.pathname.split("/").slice(1);
 const repo = path[1];
 
 if (path[2] === "pull" || path[2] === "issues") {
-  const number = path[3];
-  console.log(`[${repo}#${number}](${url})`);
+  if (path[4] === "commits") {
+    const hash = path[5];
+    console.log(`[\`${hash.substring(0, 7)}\`](${url})`);
+  } else {
+    const number = path[3];
+    console.log(`[${repo}#${number}](${url})`);
+  }
 }
 
 if (path[2] === "commit") {

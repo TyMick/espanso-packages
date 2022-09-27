91033221ae7689ad1cdef52c4f04151c07e21059
Ty Mick <github.com/TyMick/espanso-packages@tymick.email>
2022-09-27 08:56:20 -0700

    Add Jira `servicedesk` URL handling

--- jira-links/script.mjs
+++ jira-links/script.mjs
@@ -1,6 +1,20 @@
 const url = new URL(process.argv[2]);
-// Example: https://company.atlassian.net/browse/ABC-1234
-
 const path = url.pathname.split("/").slice(1);
-const jiraCase = path[1];
+
+let jiraCase;
+switch (path[0]) {
+  // Example: https://company.atlassian.net/browse/ABC-1234
+  case "browse":
+    jiraCase = path[1];
+    break;
+
+  // Example: https://company.atlassian.net/servicedesk/customer/portal/10/ABC-1234
+  case "servicedesk":
+    jiraCase = path[4];
+    break;
+
+  default:
+    throw new Error("Unknown Jira URL format");
+}
+
 console.log(`[${jiraCase}](${url})`);
